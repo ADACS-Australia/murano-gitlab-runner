9c30ee363e4877950be9c692aae78ac7f24fcc37
David Liptai <dliptai@swin.edu.au>
2021-10-19 17:08:52 +1100

    gitlab runner attempt

--- Resources/scripts/deploy.sh
+++ Resources/scripts/deploy.sh
@@ -1,23 +1,10 @@
-#!/bin/bash
-#  Licensed under the Apache License, Version 2.0 (the "License"); you may
-#  not use this file except in compliance with the License. You may obtain
-#  a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#  Unless required by applicable law or agreed to in writing, software
-#  distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
-#  WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
-#  License for the specific language governing permissions and limitations
-#  under the License.
+#!/bin/bash -xeu
 
-sudo apt-get update
-sudo apt-get -y install apache2
+RUNNER_NAME=$1
+RUNNER_URL=$2
+RUNNER_TOKEN=$3
 
-if [[ $1 == "True" ]];
-then
-    sudo apt-get -y install php5
-fi
-
-sudo iptables -I INPUT 1 -p tcp -m tcp --dport 443 -j ACCEPT -m comment --comment "by murano, Apache server access on HTTPS port 443"
-sudo iptables -I INPUT 1 -p tcp -m tcp --dport 80 -j ACCEPT -m comment --comment "by murano, Apache server access on HTTP port 80"
+sudo yum -y update
+wget https://gitlab-runner-downloads.s3.amazonaws.com/latest/rpm/gitlab-runner_amd64.rpm
+sudo rpm -i gitlab-runner_amd64.rpm
+sudo gitlab-runner register --name $RUNNER_NAME --url $RUNNER_URL --token $RUNNER_TOKEN
