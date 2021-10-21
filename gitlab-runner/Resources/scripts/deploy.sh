547eae477f9aeb719ea23a7e147f1fd593046694
David Liptai <dliptai@swin.edu.au>
2021-10-21 17:22:28 +1100

    cleaner install

--- gitlab-runner/Resources/scripts/deploy.sh
+++ gitlab-runner/Resources/scripts/deploy.sh
@@ -1,14 +1,12 @@
 #!/bin/bash -xeu
 
-export RUNNER_NAME="$1"
-export CI_SERVER_URL="$2"
-export REGISTRATION_TOKEN="$3"
-export RUNNER_EXECUTOR="shell"
-
-wget https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb
+RUNNER_NAME="$1"
+CI_SERVER_URL="$2"
+REGISTRATION_TOKEN="$3"
+RUNNER_EXECUTOR="shell"
 
 while true; do
-  sudo dpkg -i gitlab-runner_amd64.deb && break || echo "Waiting 10s for apt..."; sleep 10
+  sudo apt install -y gitlab-runner && break || echo "Waiting 10s for apt..."; sleep 10
 done
 
 sudo gitlab-runner register --non-interactive --name "$RUNNER_NAME" --url "$CI_SERVER_URL" -r "$REGISTRATION_TOKEN" --executor "$RUNNER_EXECUTOR"
