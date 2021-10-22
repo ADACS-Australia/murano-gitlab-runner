65c9ac7c0ce81f13e5ffb5b6ba7a961d3c1ca5a8
Conrad Chan <conradchan@hotmail.com.au>
2021-10-22 11:29:03 +1100

    install docker.io and use docker executor

--- gitlab-runner/Resources/scripts/deploy.sh
+++ gitlab-runner/Resources/scripts/deploy.sh
@@ -4,11 +4,11 @@ RUNNER_NAME="$1"
 CI_SERVER_URL="$2"
 REGISTRATION_TOKEN="$3"
 RUNNER_TAG_LIST="$4"
-RUNNER_EXECUTOR="shell"
+RUNNER_EXECUTOR="docker"
 
 # Wait for auto updates to finish
 while true; do
-  sudo apt-get install -y -q gitlab-runner > /dev/null && break || echo "Waiting 10s for apt..."; sleep 10
+  sudo apt-get install -y -q docker.io gitlab-runner > /dev/null && break || echo "Waiting 10s for apt..."; sleep 10
 done
 
 sudo gitlab-runner register \
