edb9c4d46b2ffd2fceac69ac049dbbd4736f5e73
Conrad Chan <conradchan@hotmail.com.au>
2021-10-22 17:16:16 +1100

    Use docker executor
    Revert back to installing gitlab-runner from deb because the apt version is very old and refers to a missing gitlab-runner-helper docker image.
    
    Use ubuntu as the default image, if none is specified.

--- gitlab-runner/Resources/scripts/deploy.sh
+++ gitlab-runner/Resources/scripts/deploy.sh
@@ -5,16 +5,26 @@ CI_SERVER_URL="$2"
 REGISTRATION_TOKEN="$3"
 RUNNER_TAG_LIST="$4"
 RUNNER_EXECUTOR="docker"
+DOCKER_IMAGE="ubuntu" # default docker image
 
-# Wait for auto updates to finish
+# Download gitlab-runner (while auto updates are running)
+wget https://gitlab-runner-downloads.s3.amazonaws.com/latest/deb/gitlab-runner_amd64.deb
+
+# Wait for auto updates to finish before installing docker
 while true; do
-  sudo apt-get install -y -q docker.io gitlab-runner > /dev/null && break || echo "Waiting 10s for apt..."; sleep 10
+  sudo apt-get install -y -q docker.io > /dev/null && break || echo "Waiting 10s for apt..."; sleep 10
 done
 
+# Install gitlab-runner
+sudo dpkg -i gitlab-runner_amd64.deb
+
+# Register runner
 sudo gitlab-runner register \
   --non-interactive \
   --name "$RUNNER_NAME" \
   --url "$CI_SERVER_URL" \
   -r "$REGISTRATION_TOKEN" \
   --executor "$RUNNER_EXECUTOR" \
-  --tag-list "$RUNNER_TAG_LIST"
+  --docker-image "$DOCKER_IMAGE" \
+  --tag-list "$RUNNER_TAG_LIST" \
+  --run-untagged
