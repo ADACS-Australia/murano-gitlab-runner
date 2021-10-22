affcff98b48a44e2c931bd68413f5537a5a2e680
David Liptai <dliptai@swin.edu.au>
2021-10-22 11:07:42 +1100

    Allow tags to be added to runner

--- gitlab-runner/Resources/scripts/deploy.sh
+++ gitlab-runner/Resources/scripts/deploy.sh
@@ -3,10 +3,18 @@
 RUNNER_NAME="$1"
 CI_SERVER_URL="$2"
 REGISTRATION_TOKEN="$3"
+RUNNER_TAG_LIST="$4"
 RUNNER_EXECUTOR="shell"
 
+# Wait for auto updates to finish
 while true; do
   sudo apt-get install -y -q gitlab-runner > /dev/null && break || echo "Waiting 10s for apt..."; sleep 10
 done
 
-sudo gitlab-runner register --non-interactive --name "$RUNNER_NAME" --url "$CI_SERVER_URL" -r "$REGISTRATION_TOKEN" --executor "$RUNNER_EXECUTOR"
+sudo gitlab-runner register \
+  --non-interactive \
+  --name "$RUNNER_NAME" \
+  --url "$CI_SERVER_URL" \
+  -r "$REGISTRATION_TOKEN" \
+  --executor "$RUNNER_EXECUTOR" \
+  --tag-list "$RUNNER_TAG_LIST"
